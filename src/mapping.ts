31e2eb7be1e4ce8d7c586291b0ea8da08f0ee905
Erik Rogers <ewrogers@gmail.com>
2021-03-20 01:23:19 -0400

    feat: use event instead of call

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,56 +1,32 @@
-import { TransferCall, TransferFromCall } from './types/SimianToken/SimianToken'
+import { BigDecimal } from "@graphprotocol/graph-ts"
+import { Transfer as TransferEvent } from './types/SimianToken/SimianToken'
 import { Transfer } from './types/schema'
-import {BigDecimal} from "@graphprotocol/graph-ts"
 import { convertTokenToDecimal } from "./helpers"
 
+const NINETY_FIVE_PERCENT = BigDecimal.fromString("0.95")
 const FIVE_PERCENT = BigDecimal.fromString("0.05")
 
-// Handles a Transfer call to the token contract
-export function handleTransfer(call: TransferCall): void {
+// Handles a Transfer event from the token contract
+export function handleTransfer(event: TransferEvent): void {
   // The ID will be the transaction hash since that is likely unique to each transfer
-  const id = call.transaction.hash.toHexString()
+  const id = event.transaction.hash.toHexString()
 
   let transfer = Transfer.load(id)
   if (transfer == null) {
     transfer = new Transfer(id)
   }
 
-  transfer.transaction = call.transaction.hash
-  transfer.blockNumber = call.block.number
-  transfer.timestamp = call.block.timestamp
+  transfer.transaction = event.transaction.hash
+  transfer.blockNumber = event.block.number
+  transfer.timestamp = event.block.timestamp
 
-  transfer.from = call.from
-  transfer.to = call.inputs.recipient
-
-  // Calculate the original amount, fee amount, and net transfer amount
-  transfer.amount = convertTokenToDecimal(call.inputs.amount)
-  transfer.feeAmount = transfer.amount.times(FIVE_PERCENT)
-  transfer.transferAmount = transfer.amount.minus(transfer.feeAmount)
-
-  transfer.save()
-}
-
-// Handles a TransferFrom call to the token contract
-export function handleTransferFrom(call: TransferFromCall): void {
-  // The ID will be the transaction hash since that is likely unique to each transfer
-  const id = call.transaction.hash.toHexString()
-
-  let transfer = Transfer.load(id)
-  if (transfer == null) {
-    transfer = new Transfer(id)
-  }
-
-  transfer.transaction = call.transaction.hash
-  transfer.blockNumber = call.block.number
-  transfer.timestamp = call.block.timestamp
-
-  transfer.from = call.inputs.sender
-  transfer.to = call.inputs.recipient
+  transfer.from = event.params.from
+  transfer.to = event.params.to
 
   // Calculate the original amount, fee amount, and net transfer amount
-  transfer.amount = convertTokenToDecimal(call.inputs.amount)
+  transfer.transferAmount = convertTokenToDecimal(event.params.value)
+  transfer.amount = transfer.transferAmount.div(NINETY_FIVE_PERCENT)
   transfer.feeAmount = transfer.amount.times(FIVE_PERCENT)
-  transfer.transferAmount = transfer.amount.minus(transfer.feeAmount)
 
   transfer.save()
 }
