f256c02b143ce29fb9eb3f0b5d9d08bf5a9c0a26
Erik Rogers <ewrogers@gmail.com>
2021-03-20 01:05:48 -0400

    feat: add transferFrom tracking

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,11 +1,11 @@
-import { TransferCall } from './types/SimianToken/SimianToken'
+import { TransferCall, TransferFromCall } from './types/SimianToken/SimianToken'
 import { Transfer } from './types/schema'
 import {BigDecimal} from "@graphprotocol/graph-ts"
 import { convertTokenToDecimal } from "./helpers"
 
 const FIVE_PERCENT = BigDecimal.fromString("0.05")
 
-// Handles a Transfer event from the token contract
+// Handles a Transfer call to the token contract
 export function handleTransfer(call: TransferCall): void {
   // The ID will be the transaction hash since that is likely unique to each transfer
   const id = call.transaction.hash.toHexString()
@@ -20,6 +20,31 @@ export function handleTransfer(call: TransferCall): void {
   transfer.timestamp = call.block.timestamp
 
   transfer.from = call.from
+  transfer.to = call.inputs.recipient
+
+  // Calculate the original amount, fee amount, and net transfer amount
+  transfer.amount = convertTokenToDecimal(call.inputs.amount)
+  transfer.feeAmount = transfer.amount.times(FIVE_PERCENT)
+  transfer.transferAmount = transfer.amount.minus(transfer.feeAmount)
+
+  transfer.save()
+}
+
+// Handles a TransferFrom call to the token contract
+export function handleTransferFrom(call: TransferFromCall): void {
+  // The ID will be the transaction hash since that is likely unique to each transfer
+  const id = call.transaction.hash.toHexString()
+
+  let transfer = Transfer.load(id)
+  if (transfer == null) {
+    transfer = new Transfer(id)
+  }
+
+  transfer.transaction = call.transaction.hash
+  transfer.blockNumber = call.block.number
+  transfer.timestamp = call.block.timestamp
+
+  transfer.from = call.inputs.sender
   transfer.to = call.inputs.recipient
 
   // Calculate the original amount, fee amount, and net transfer amount
