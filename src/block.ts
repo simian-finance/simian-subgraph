be829820d8bc5c6bce3f00a0e2f91b1172ed22fc
Erik Rogers <ewrogers@gmail.com>
2021-03-21 20:43:57 -0400

    refactor: organize code into token function

--- src/block.ts
+++ src/block.ts
@@ -1,9 +1,6 @@
-import { Address, ethereum } from '@graphprotocol/graph-ts'
-import { Contract } from "./types/Contract/Contract"
-import { getTokenInstance } from "./token"
+import { ethereum } from '@graphprotocol/graph-ts'
+import { updateTokenSupply } from "./token"
 import { updateAccountBalances } from "./accounts"
-import { convertTokenToDecimal } from "./helpers"
-import { BURN_ADDRESS, CONTRACT_ADDRESS } from "./constants"
 
 // This is invoked every block, after all event and call handlers
 export function handleBlock(block: ethereum.Block ) : void {
@@ -12,17 +9,6 @@ export function handleBlock(block: ethereum.Block ) : void {
 
 // This is invoked every block that has a call to the SIFI contract, after all event and call handlers
 export function handleBlockWithContractCall(block: ethereum.Block ) : void {
-  let contract = Contract.bind(Address.fromString(CONTRACT_ADDRESS))
-  let token = getTokenInstance()
-
-  // Get the amount burned
-  let burnAddress = Address.fromString(BURN_ADDRESS)
-  let totalBurned = convertTokenToDecimal(contract.balanceOf(burnAddress))
-
-  // Determine remaining supply from burned
-  token.totalBurned = totalBurned
-  token.remainingSupply = token.totalSupply.minus(totalBurned)
-  token.save()
-
+  updateTokenSupply()
   updateAccountBalances()
 }
--- src/token.ts
+++ src/token.ts
@@ -1,7 +1,8 @@
 import { Address } from '@graphprotocol/graph-ts'
 import { Contract } from './types/Contract/Contract'
 import { Token } from './types/schema'
-import { CONTRACT_ADDRESS, DECIMAL_ZERO, INT_ZERO } from "./constants"
+import { convertTokenToDecimal } from "./helpers"
+import { BURN_ADDRESS, CONTRACT_ADDRESS, DECIMAL_ZERO, INT_ZERO } from "./constants"
 
 // Gets the singleton instance of the token entity
 export function getTokenInstance() : Token {
@@ -26,4 +27,21 @@ export function getTokenInstance() : Token {
   }
 
   return token as Token
+}
+
+export function updateTokenSupply() : void {
+  let contract = Contract.bind(Address.fromString(CONTRACT_ADDRESS))
+  let token = getTokenInstance()
+
+  // Get the amount burned
+  let burnAddress = Address.fromString(BURN_ADDRESS)
+  let totalBurned = convertTokenToDecimal(contract.balanceOf(burnAddress))
+
+  // Determine remaining supply from burned
+  token.totalBurned = totalBurned
+  token.remainingSupply = token.totalSupply.minus(totalBurned)
+
+  // Update total fees
+  token.totalFees = convertTokenToDecimal(contract.totalFees())
+  token.save()
 }
